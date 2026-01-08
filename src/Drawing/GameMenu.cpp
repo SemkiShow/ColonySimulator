1ad7bf72c15ff9690937ed387668d519bdadc689
SemkiShow <mgdeveloper123@gmail.com>
2026-01-08 22:49:20 +0100

    Fix ship texture

--- src/Drawing/GameMenu.cpp
+++ src/Drawing/GameMenu.cpp
@@ -136,8 +136,8 @@ void DrawGameMenu()
         Vector2 pos = GlslToRaylib(ship.pos);
         DrawTexturePro(shipTexture, {0, 0, shipTexture.width * 1.0f, shipTexture.height * 1.0f},
                        {pos.x, pos.y, shipTexture.width * scale, shipTexture.height * scale},
-                       {shipTexture.width * scale / 2.0f, shipTexture.height * scale / 2.0f}, 0,
-                       WHITE);
+                       {shipTexture.width * scale / 2.0f, shipTexture.height * scale}, 0, WHITE);
+        // DrawRectangle(pos.x - 10, pos.y - 20, 20, 20, Color{127, 127, 127, 127});
     }
 
     if (showIslandsBoxes)
