3ba0f3b9d26027b031f95827150959b974f4b00a
SemkiShow <mgdeveloper123@gmail.com>
2026-01-31 16:47:35 +0100

    Credit jaraslauzaitsau for the music

--- src/UI/About.cpp
+++ src/UI/About.cpp
@@ -5,6 +5,7 @@
 #include "UI/About.hpp"
 #include "Languages.hpp"
 #include <RWidgets/RLabel.hpp>
+#include <RWidgets/RScrollArea.hpp>
 #include <RWidgets/RVBoxLayout.hpp>
 
 std::shared_ptr<AboutMenu> aboutMenu;
@@ -18,11 +19,24 @@ AboutMenu::AboutMenu()
     title->SetAlignment(RAlign::HCenter);
     layout->AddWidget(title);
 
-    auto credits =
-        std::make_shared<RLabel>(_("Lead Developer: SemkiShow\nDeveloper: jaraslauzaitsau"), 32);
-    credits->SetAlignment(RAlign::HCenter);
-    credits->SetAlignment(RAlign::VCenter);
-    layout->AddWidget(credits);
+    auto scrollArea = std::make_shared<RScrollArea>();
+    layout->AddWidget(scrollArea);
+
+    auto creditsLayout = std::make_shared<RVBoxLayout>();
+    scrollArea->SetCentralWidget(creditsLayout);
+
+    auto addLabel = [creditsLayout](const std::string& text, int textSize)
+    {
+        auto label = std::make_shared<RLabel>(text, textSize);
+        label->SetAlignment(RAlign::HCenter);
+        label->SetAlignment(RAlign::VCenter);
+        creditsLayout->AddWidget(label);
+    };
+
+    addLabel(_("Programming:"), 48);
+    addLabel(_("SemkiShow\njaraslauzaitsau"), 32);
+    addLabel(_("Music:"), 48);
+    addLabel(_("jaraslauzaitsau"), 32);
 
     auto license = std::make_shared<RLabel>(_("This game is licensed under GPL v3.0"), 24);
     license->SetAlignment(RAlign::HCenter);
